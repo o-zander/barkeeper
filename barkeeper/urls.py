ce8f91b153b32683ce2c56de90e913b3286fff0b
Oliver Zander <zander@cosmocode.de>
2014-07-05 17:25:13 +0200

    added recipes & ingredients views

--- barkeeper/urls.py
+++ barkeeper/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from .views import IndexView as index
+from .views import IndexView, RecipesView, IngredientsView
 
 urlpatterns = patterns(
     'barkeeper.views',
 
-    url(r'^$', index.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^recipes/$', RecipesView.as_view(), name='recipes'),
+    url(r'^ingredients/$', IngredientsView.as_view(), name='ingredients'),
 
     # Admin
     url(r'^admin/', include(admin.site.urls)),
--- barkeeper/views.py
+++ barkeeper/views.py
@@ -1,5 +1,25 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Recipe, Ingredient
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class RecipesView(ListView):
+    model = Recipe
+    template_name = 'recipe/list.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs.update(view_name='recipes')
+        return super(RecipesView, self).get_context_data(**kwargs)
+
+
+class IngredientsView(ListView):
+    model = Ingredient
+    template_name = 'ingredient/list.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs.update(view_name='ingredients')
+        return super(IngredientsView, self).get_context_data(**kwargs)
